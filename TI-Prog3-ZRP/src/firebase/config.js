0a9e50eb0d05d2a673f3f865986e4fb1c61b071e
TZippilli <tzippilli@udesa.edu.ar>
2024-11-05 22:24:13 -0300

    cambio mi config

--- TI-Prog3-ZRP/src/firebase/config.js
+++ TI-Prog3-ZRP/src/firebase/config.js
@@ -2,13 +2,13 @@ import app from 'firebase/app';
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBmHxq8UuSY4c3d2To0c__w4KtgevzDcR0",
-    authDomain: "ti-prog3-zrp.firebaseapp.com",
-    projectId: "ti-prog3-zrp",
-    storageBucket: "ti-prog3-zrp.firebasestorage.app",
-    messagingSenderId: "957249261616",
-    appId: "1:957249261616:web:495a063d2c94059901ddf5"
-  };
+  apiKey: "AIzaSyAqMFSXafyO_P6CQASoUtwQ1uo07hg6KXE",
+  authDomain: "ti-prog3-zrp-75e37.firebaseapp.com",
+  projectId: "ti-prog3-zrp-75e37",
+  storageBucket: "ti-prog3-zrp-75e37.firebasestorage.app",
+  messagingSenderId: "949725724648",
+  appId: "1:949725724648:web:d38f173491fd880d312e5d"
+};
 
 app.initializeApp(firebaseConfig);
 
