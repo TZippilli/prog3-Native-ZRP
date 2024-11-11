d9a53911e056c9f45bfa6dd6b82d5fe413188dd2
TZippilli <tzippilli@udesa.edu.ar>
2024-11-11 17:52:42 -0300

    cambio Post a class comp

--- TI-Prog3-ZRP/src/components/Post.js
+++ TI-Prog3-ZRP/src/components/Post.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
+export default class Post extends Component {
+  render() {
+    const { email, descripcion } = this.props;
 
-export default function Post({ email, descripcion }) {
     return (
-        <View style={styles.container}>
-            <Text>Email: {email}</Text>
-            <Text>Mensaje: {descripcion}</Text>
-        </View>
+      <View style={styles.container}>
+        <Text>Email: {email}</Text>
+        <Text>Mensaje: {descripcion}</Text>
+      </View>
     );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
